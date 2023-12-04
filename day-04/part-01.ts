1454418f4082eee512de43512df9efd0ce2bb052
Dave Cooper <dave.cooper@ourfuturehealth.org.uk>
2023-12-04 19:56:55 +0000

    Day 4 - part 1

---
+++ day-04/part-01.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+
+const answer = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
+  .split("\n")
+  .map((line) =>
+    line
+      .replace(/(^.*: )/, "")
+      .trim()
+      .split(" | ")
+      .map((item) => new Set(item.match(/(\d+)/g)!.map(Number)))
+  )
+  .reduce((acc, [ticket, winners]) => {
+    const intersection = new Set([...ticket].filter((i) => winners.has(i)));
+
+    return intersection.size === 0
+      ? acc
+      : acc +
+          Math.pow(
+            2,
+            new Set([...ticket].filter((i) => winners.has(i))).size - 1
+          );
+  }, 0);
+
+console.log(answer);
