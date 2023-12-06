9371fe5c6e49eae71c4c69fbb524fa88dfd243c4
Dave Cooper <dave.cooper@ourfuturehealth.org.uk>
2023-12-06 13:17:23 +0000

    Day 6 - part 2

---
+++ day-06/part-02.ts
@@ -0,0 +1,25 @@
+import fs from "fs";
+import path from "path";
+
+const [times, distance] = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
+  .split("\n")
+  .map((line) => line.replace(/ /g, "").match(/\d+/g)!.map(Number));
+
+const winningStrategies: number[] = [];
+
+for (let i = 0; i < times.length; i++) {
+  const time = times[i];
+  const recordDistance = distance[i];
+  let val = 0;
+
+  for (let j = 0; j < time; j++) {
+    if (j * (time - j) > recordDistance) {
+      val++;
+    }
+  }
+
+  winningStrategies.push(val);
+}
+
+console.log(winningStrategies.reduce((a, b) => a * b, 1));
